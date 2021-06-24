fe4b746103f2ade6bebcdeb876b94bd1dfbeeed6
mikaeldiok <mikaeldiok@gmail.com>
2021-06-24 19:35:32 +0700

    update composer

---
+++ config/repository.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'paths' => [
+        'model_path' => 'App/Model',
+        'repository' => 'Repositories/Eloquent',
+        'repository_interface' => 'Repositories/Contract',
+        'provider' => 'RepositoryServiceProvider'
+    ]
+];
