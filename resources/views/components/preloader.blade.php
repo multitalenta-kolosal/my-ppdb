d55f447d659d43f0e73bf8fde54e55af07918ada
mikaeldiok <mikaeldiok@gmail.com>
2021-08-05 08:23:48 +0700

    loader color

--- resources/views/components/preloader.blade.php
+++ resources/views/components/preloader.blade.php
@@ -1,6 +1,6 @@
 <div class="preloader bg-soft flex-column justify-content-center align-items-center">
     <div class="loader-element">
-        <span class="loader-animated-dot bg-indigo"></span>
+        <span class="loader-animated-dot bg-warning"></span>
         <img src="{{asset('img/logo-with-text-dark.png')}}" height="40" alt="{{app_name()}}">
     </div>
 </div>
