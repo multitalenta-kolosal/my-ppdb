ddd6710b84328f898e67b4f9674fbc103d551d24
mikaeldiok <mikaeldiok@gmail.com>
2021-10-08 11:15:04 +0700

    change bankaccount to string

--- Modules/Referal/Database/Migrations/2021_10_07_113859_create_referees_table.php
+++ Modules/Referal/Database/Migrations/2021_10_07_113859_create_referees_table.php
@@ -20,7 +20,7 @@ class CreateRefereesTable extends Migration
             $table->string('phone');
             $table->string('bank_code');
             $table->string('bank_name');
-            $table->integer('bank_account');
+            $table->string('bank_account');
             $table->string('ref_code');
             $table->timestamps();
             $table->integer('created_by')->unsigned()->nullable();
