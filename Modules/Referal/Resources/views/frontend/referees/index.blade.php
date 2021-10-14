bbbfc7f1b8a896f016fbc13257e7f2cdba3e32e1
mikaeldiok <mikaeldiok@gmail.com>
2021-10-14 18:47:56 +0700

    latest edit for referal

--- Modules/Referal/Resources/views/frontend/referees/index.blade.php
+++ Modules/Referal/Resources/views/frontend/referees/index.blade.php
@@ -73,39 +73,13 @@
                             </form>
                         </div>
                     </div>
-                    <div class="col-sm-10">
-                        <strong>--atau--</strong>
-                    </div>
-                     <a href="#form_referee" class="btn btn-danger btn-sm m-2 rounded-pill px-5" title="pendaftaran">Daftar</a>
                 </div>
 
                 @include('frontend.includes.messages')
             </div>
         </div>
     </div>
-    <div class="pattern-soft bottom"></div>
-</section>
-
-<section class="section section-lg line-bottom-light bg-light">
-    <div class="container mt-n7 mt-lg-n12 z-2">
-        <div class="row">
-             
-            <div class="col-lg-12 mb-5" id="form_referee"> 
-                <div class="card bg-white border-light shadow-soft flex-md-row no-gutters p-4">
-                    <div class="card-body d-flex flex-column justify-content-between col-auto py-4 p-lg-5">
-                        <H3 class="text-center mb-4">
-                            Pendaftaran Referee
-                        </H3>
-                        @include('referal::frontend.referees.create')
-                    </div>
-                </div>
-            </div>
-
-        </div>
-
-        <div class="d-flex justify-content-center w-100 mt-3">
-        </div>
-    </div>
+    <div class="pattern bottom"></div>
 </section>
 
 @endsection
--- Modules/Referal/Resources/views/frontend/referees/refarea.blade.php
+++ Modules/Referal/Resources/views/frontend/referees/refarea.blade.php
@@ -15,7 +15,6 @@
                     Lihat Link Referal dan Lihat Perkembangan Referal Anda
                 </p>
 
-                @include('frontend.includes.messages')
             </div>
         </div>
     </div>
@@ -40,41 +39,18 @@
                                 <div class="text-center">
                                     <span class="display-4">Link Referal:</span>
                                 </div>
-                                <div class="row py-1 my-3 shadow border border-primary rounded text-center justify-content-center">
+                                <div class="row py-3 my-3 shadow border border-primary rounded text-center justify-content-center">
                                     <div class="col">
                                         <div class="row align-middle">
                                             <div class="col text-center">
-                                                    <strong class="display-5 text-info">
-                                                        <a href="{{url('/?ref='.$referee->data->ref_code)}}">
-                                                            <span id="reflink-home-target" style="position:relative">
-                                                                <u>{{url('/?ref='.$referee->data->ref_code)}}</u>
-                                                            </span>
-                                                        </a>
-                                                    </strong>
-                                                    <br>
-                                                <button class="btn btn-sm btn-primary" id="reflink-home-btn"><i class='fas fa-copy mr-1'></i>Copy</button>
-                                            </div>
-                                        </div>
-                                        <div class="row">
-                                            <div class="col text-center">
-                                                <strong class=" text-dark">
-                                                        <span id="atau" style="position:relative">
-                                                            Atau
+                                                <strong class="display-5 text-info">
+                                                    <a href="{{url('/?ref='.$referee->data->ref_code)}}">
+                                                        <span id="reflink-home-target" style="position:relative">
+                                                            <u>{{url('/?ref='.$referee->data->ref_code)}}</u>
                                                         </span>
+                                                    </a>
                                                 </strong>
-                                            </div>
-                                        </div>
-                                        <div class="row align-middle">
-                                            <div class="col text-center">
-                                                    <strong class="display-5 text-info">
-                                                        <a href="{{url('daftar/?ref='.$referee->data->ref_code)}}">
-                                                            <span id="reflink-daftar-target" style="position:relative">
-                                                                <u>{{url('daftar/?ref='.$referee->data->ref_code)}}</u>
-                                                            </span>
-                                                        </a>
-                                                    </strong>
-                                                    <br>
-                                                <button class="btn btn-sm btn-primary" id="reflink-daftar-btn"><i class='fas fa-copy mr-1'></i>Copy</button>
+                                                <button class="btn btn-sm btn-danger" id="reflink-home-btn"><i class='fas fa-copy mr-1'></i>Copy</button>
                                             </div>
                                         </div>
                                     </div>
