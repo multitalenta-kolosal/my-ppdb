d896556bd8ab1b8740917b816e7ed671a2f628cc
mikaeldiok <mikaeldiok@gmail.com>
2021-10-23 15:22:39 +0700

    proccessing to submit

--- Modules/Registrant/Resources/views/frontend/registrants/create.blade.php
+++ Modules/Registrant/Resources/views/frontend/registrants/create.blade.php
@@ -3,7 +3,7 @@
     $module_name = 'registrants';
     $module_action = 'Create';
     ?>
-    {{ html()->form('POST', route("frontend.$module_name.store"))->class('form')->open() }}
+    {{ html()->form('POST', route("frontend.$module_name.store"))->id("reg")->class('form')->open() }}
 
     @include('registrant::frontend.registrants.form',['module_name' => 'registrants'])
 
@@ -11,12 +11,32 @@
         <div class="col-12">
             <div class="float-right">
                 <div class="form-group">
-                {{ html()->button($text = "<i class='fas fa-long-arrow-alt-right'></i> " . ucfirst("Daftar") . "", $type = 'submit')->class('btn btn-success btn-lg') }}
+                {{ html()->button($text = "<i class='fas fa-long-arrow-alt-right'></i> " . ucfirst("Daftar") . "", $type = 'submit')->id("submit-reg")->class('btn btn-success btn-lg') }}
                 </div>
             </div>
         </div>
     </div>
 
     {{ html()->form()->close() }}
-
+    
 </div>
+
+@push('after-scripts')
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        $("#reg").on("submit", function (event) {
+            $("#submit-reg").attr("disabled", true);
+            Swal.fire({
+                title: 'Sedang Diproses...',
+                html: 'Jangan tutup atau refresh halaman ini.',
+                allowOutsideClick: false
+                didOpen: () => {
+                    Swal.showLoading();
+                },
+            });
+        });
+    });
+</script>
+
+@endpush
