ca2f29d4385be07b6c973dd399e0a7a72a80b3bf
mikaeldiok <mikaeldiok@gmail.com>
2023-09-02 22:45:49 +0700

    png to webp

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -8,13 +8,13 @@
     <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
     <div class="carousel-inner">
         <div class="carousel-item active">
-        <img class="d-block w-100" src="/img/content/car1.png" alt="First slide">
+        <img class="d-block w-100" src="/img/content/car1.webp" alt="First slide">
         </div>
         <div class="carousel-item">
-        <img class="d-block w-100" src="/img/content/car2.png" alt="Second slide">
+        <img class="d-block w-100" src="/img/content/car2.webp" alt="Second slide">
         </div>
         <div class="carousel-item">
-        <img class="d-block w-100" src="/img/content/car3.png" alt="Third slide">
+        <img class="d-block w-100" src="/img/content/car3.webp" alt="Third slide">
         </div>
     </div>
     </div>
@@ -36,7 +36,7 @@
 
 <section class="section section-ld bg-body" id="form_pendaftaran">
     <div class="container">
-        <img class="d-block my-3" src="/img/content/jalur_istimewa.png" alt="Jalur Istimewa">
+        <img class="d-block my-3" src="/img/content/jalur_istimewa.webp" alt="Jalur Istimewa">
         <div class="row">
             @include('flash::message')
             <div class="col-12 text-center">
