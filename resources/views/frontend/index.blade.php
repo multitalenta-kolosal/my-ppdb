8aea1beadf74b6344e10ccabd6c3e4594ac61627
mikaeldiok <mikaeldiok@gmail.com>
2023-09-04 08:41:23 +0700

    tampilan

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -6,17 +6,17 @@
 
 <section class="section-header pb-6 pb-lg-6 warga-purple text-white">
     <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
-    <div class="carousel-inner">
-        <div class="carousel-item active">
-        <img class="d-block w-100" src="/img/content/car1.webp" alt="First slide">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img class="d-block w-100" src="/img/content/car1.webp" alt="First slide">
+            </div>
+            <div class="carousel-item">
+            <img class="d-block w-100" src="/img/content/car2.webp" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+            <img class="d-block w-100" src="/img/content/car3.webp" alt="Third slide">
+            </div>
         </div>
-        <div class="carousel-item">
-        <img class="d-block w-100" src="/img/content/car2.webp" alt="Second slide">
-        </div>
-        <div class="carousel-item">
-        <img class="d-block w-100" src="/img/content/car3.webp" alt="Third slide">
-        </div>
-    </div>
     </div>
     <div class="container">
         <div class="row justify-content-center">
